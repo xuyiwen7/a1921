bf6cc129a6bc5f1a18b56407ac178b3492a05c50
xuyiwen7 <git@github.com>
2020-11-19 22:33:49 +0800

    first commit

---
+++ makefile
@@ -0,0 +1,4 @@
+#homework
+text:lianbiao.c  testlianbiao.c  yuesefu.h
+	gcc -g lianbiao.c testlianbiao.c yuesefu.h -o text 
+
---
+++ testlianbiao.c
@@ -0,0 +1,22 @@
+#include "yuesefu.h"
+int main(){
+	LinkList *list=CreateList();
+	Insert(list,1,10);
+	Insert(list,1,20);
+	Felete(list,2);
+	Insert(list,1,30);
+	Insert(list,1,40);
+	printf("链表的元素个数为：%d\n", Size(list));
+	Print(list);
+	printf("链表的第二个元素为：%d",GetData(list,2));
+
+
+
+
+
+
+
+	    #define n 11
+	      #define m 3
+		yuesefu ();	       
+}
